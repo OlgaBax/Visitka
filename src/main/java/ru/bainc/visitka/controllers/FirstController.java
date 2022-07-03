8e76309b5c90c5a12401b553a65509a6052131ee
Babushkina Olga <ba80@mail.ru>
2022-07-03 13:14:23 +0300

    Initial commit

---
+++ src/main/java/ru/bainc/visitka/controllers/FirstController.java
@@ -0,0 +1,14 @@
+package ru.bainc.visitka.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class FirstController {
+@GetMapping
+    public String helloPage(){
+    return"hello";
+}
+}
